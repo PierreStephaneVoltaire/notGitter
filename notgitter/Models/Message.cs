7970efddcce6a0be78450bc880d335e9cb8af867
Kevin W <weiyih@sheridancollege.ca>
2018-01-07 17:00:49 -0500

    revert

--- notgitter/Models/Message.cs
+++ notgitter/Models/Message.cs
@@ -17,6 +17,7 @@ namespace notgitter.Models
         public long Id { get; set; }
         public string Content { get; set; }
         public Nullable<System.DateTime> timestamp { get; set; }
+        public string UserName { get; set; }
         public long RepoId { get; set; }
         public int Uid { get; set; }
     
